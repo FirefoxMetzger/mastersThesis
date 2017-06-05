7e9a9b8e0cc8f8f95f25e509b4b78e44d2d0e6e7
FirefoxMetzger <sebastian@wallkoetter.net>
2017-06-05 23:17:17 +0100

    moved environment into seperate file

--- Agent.py
+++ Agent.py
@@ -2,58 +2,51 @@ import zmq
 import thread
 import numpy as np
 from optparse import OptionParser
+from logging import Logger
 
 class Agent(object):
-    def __init__(self, observation_space, action_space):
+    def __init__(self, socket_ip):
         self.context = zmq.Context()
         self.recieve_commands = True
-        self.recieve_step = True
+        self.socket_ip = socket_ip
 
         self.observation_space = observation_space
         self.action_space = action_space
         self.reward = 0
         
         thread.start_new_thread(self.commandThread,(self.context,))
-        thread.start_new_thread(self.stepThread,(self.context,))
 
     def commandThread(self, context):
         socket = context.socket(zmq.REP)
-        socket.bind("tcp://127.0.0.1:12346")
+        socket.bind(self.socket_ip)
 
         while self.recieve_commands:
             cmd = socket.recv_json()
             cmd_name = cmd[0]
+            msg = "OK"
             
             if cmd_name == "seed":
                 self.seed(cmd[1])
-                msg = "OK"
             elif cmd_name == "reset":
                 action =  self.reset(cmd[1])
                 msg = action
             elif cmd_name == "close":
                 self.close()
-                msg="OK"
+            elif cmd_name == "step":
+                msg = list()
+
+                observation = np.asarray(msg[0])
+                reward = msg[1]
+                done = msg[2]
+
+                action = self.step(observation, reward, done)
+                msg.append(action)
+            elif cmd_name == "setObservation":
+                self.observation_space = msg[1]
+            elif cmd_name == "setActions":
+                self.action_space = msg[1]
 
             socket.send_json(msg)
-
-        socket.close()
-
-    def stepThread(self, context):
-        socket = context.socket(zmq.REP)
-        socket.bind("tcp://127.0.0.1:12345")
-
-        while self.recieve_step:
-            msg = socket.recv_json()
-            return_msg = list()
-
-            observation = np.asarray(msg[0])
-            reward = msg[1]
-            done = msg[2]
-
-            action = self.step(observation, reward, done)
-            return_msg.append(action)
-
-            socket.send_json(action)
 
         socket.close()
 
@@ -73,7 +66,6 @@ class Agent(object):
 
     def close(self):
         self.recieve_commands = False
-        self.recieve_step = False
 
 if __name__ == "__main__":
     # run agent as standalone
@@ -95,5 +87,23 @@ if __name__ == "__main__":
     (options, args) = parser.parse_args()
 
     print options
+    if options.version:
+        return "0.0.1"
+    
     if options.verbose and options.quiet:
         parser.error("-v and -q are mutually exclusive")
+
+    socket_ip = "tcp://%s:%s" % (options.ip, options.port)
+
+    agent = Agent(socket_ip)
+    
+    alive = True
+    while alive:
+        try:
+            sleep(0.1)
+        except KeyboardInterrupt:
+            alive = False
+
+    
+
+    
--- testEnv.py
+++ testEnv.py
@@ -1,7 +1,5 @@
 import random
-import gym
 import zmq
-from Agent import Agent
 
 random.seed(19930802)
 
@@ -13,53 +11,67 @@ random_seed_agent = [random.random() for s in range(1,num_trials+1)]
 max_steps = 10000
 
 context = zmq.Context()
-action_socket = context.socket(zmq.REQ)
-action_socket.connect("tcp://127.0.0.1:12345")
+agent = context.socket(zmq.REQ)
+agent.connect("tcp://127.0.0.1:12345")
 
-command_socket = context.socket(zmq.REQ)
-command_socket.connect("tcp://127.0.0.1:12346")
+env = context.socket(zmq.REQ)
+env.connect("tcp://localhost:4567")
 
 env_name="CartPole-v0"
-env = gym.make(env_name)
+msg = ["load", env_name]
+env.send_json(msg)
+env.recv_json()
 
-agent = Agent(env.observation_space, env.action_space)
+obs_space = env.send_json(["getObservation"])
+env.recv_json()
+
+msg = ["setObservation"]
+msg.append(obs_space)
+agent.send_json(msg)
+agent.recv_json()
+
+act_space = env.send_json(["getActions"])
+env.recv_json()
+msg = ["getActions"]
+msg.append(act_space)
+agent.send_json(msg)
+agent.recv_json()
 
 for trial in range(1,num_trials+1):
     #reset environment
-    env.seed(random_seed_environment[trial-1])
-    observation = env.reset()
+    msg = ["seed", random_seed_environment[trial-1]]
+    env.send_json(msg)
+    env.recv_json()
+
+    msg = ["reset"]
+    env.send_json(msg)
+    observation = env.recv_json()
+    
     reward = 0
     done = False
 
     #reset agent
-    cmd = list()
-    cmd.append("seed")
-    cmd.append(random_seed_agent[trial-1])
-    command_socket.send_json(cmd)
-    command_socket.recv_json()
+    cmd = ["seed", random_seed_agent[trial-1]]
+    agent.send_json(cmd)
+    agent.recv_json()
 
-    cmd = list()
-    cmd.append("reset")
-    cmd.append(observation.tolist())
-    
+    cmd = ["reset", observation]
     command_socket.send_json(cmd)
     action = command_socket.recv_json()
 
     while not done:
         # update environment
-        [observation, reward, done, info] = env.step(action)
-        env.render()
-
-        msg = list()
-        msg.append(observation.tolist())
-        msg.append(reward)
-        msg.append(done)
+        env.send_json(["step", action])
+        [observation, reward, done, info] = env.recv_json(action)
 
         #update agent
-        action_socket.send_json(msg)
-        action = action_socket.recv_json()
+        msg = [observation, reward, done]
+        agent.send_json(msg)
+        action = agent.recv_json()
 
-env.close()
+msg = ["close"]
+env.send_json(msg)
+env.recv_json()
 
 msg = list()
 msg.append("close")
