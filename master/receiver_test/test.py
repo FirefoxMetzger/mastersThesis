6977e645034b4ba06ffb5399361910c89da5569c
Sebastian Wallkoetter <sebastian@wallkoetter.net>
2017-06-22 15:14:02 +0100

    stated SQL
    started connecting to an SQL database to update the results once work
    is done. This is the first step in automating the parallelism

--- master/receiver_test/test.py
+++ master/receiver_test/test.py
@@ -3,6 +3,7 @@ import logging
 import Queue
 import os
 import sys
+import MySQLdb
 
 # setup logger
 logger = logging.getLogger()
@@ -16,6 +17,11 @@ logger.debug("Set logging level to %s" % os.environ["LOGGING_LEVEL"])
 
 # local variables
 context = zmq.Context()
+connection = MySQLdb.connect(host="mysql",
+        user="root",
+        passwd="password",
+        db="dummy")
+x = connection.cursor()
 
 # setup sockets
 result_address = "tcp://" + os.environ["RESULT_ADDRESS"]
@@ -31,12 +37,18 @@ try:
         msg = result_server.recv_json()        
         # store result (or print it =) ) 
         logger.info("Storing: %s" % msg)
+        try:
+            x.execute("""UPDATE dummy SET result=%s WHERE ID=%s""",(msg, task_id))
+            connection.commit()
+        except:
+            conn.rollback()
+
 
         logger.debug("Sending reply: success")
         result_server.send_json("success")
 except KeyboardInterrupt:
     logger.info("Keyboard interrupt. Shutting down.")
 finally:
+    conn.close() 
     result_server.unbind(result_address)
     context.term()
-            
