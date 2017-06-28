477f5ed7a8e752dd68df7609276215454080dddd
Sebastian Wallkoetter <sebastian@wallkoetter.net>
2017-06-28 12:31:18 +0100

    added MySQL peewee

--- master/receiver_test/test.py
+++ master/receiver_test/test.py
@@ -3,7 +3,8 @@ import logging
 import Queue
 import os
 import sys
-import MySQLdb
+import peewee
+import playhouse.db_url
 
 # setup logger
 logger = logging.getLogger()
@@ -15,13 +16,23 @@ logger.addHandler(handler)
 logger.setLevel(os.environ["LOGGING_LEVEL"])
 logger.debug("Set logging level to %s" % os.environ["LOGGING_LEVEL"])
 
+# setup MySQL
+ip = os.environ["MYSQL_ADDRESS"]
+db = playhouse.db_url.connect(ip)
+
+class Experiment(peewee.Model):
+    a = peewee.DoubleField()
+    b = peewee.DoubleField()
+    result = peewee.DoubleField(null=True)
+    last_modified = peewee.TimestampField()
+    class Meta:
+        database = db
+
+if not Experiment.table_exists():
+    db.create_tables([Experiment])
+
 # local variables
 context = zmq.Context()
-connection = MySQLdb.connect(host="mysql",
-        user="root",
-        passwd="password",
-        db="dummy")
-x = connection.cursor()
 
 # setup sockets
 result_address = "tcp://" + os.environ["RESULT_ADDRESS"]
@@ -37,18 +48,12 @@ try:
         msg = result_server.recv_json()        
         # store result (or print it =) ) 
         logger.info("Storing: %s" % msg)
-        try:
-            x.execute("""UPDATE dummy SET result=%s WHERE ID=%s""",(msg, task_id))
-            connection.commit()
-        except:
-            conn.rollback()
-
+        msg.save()
 
         logger.debug("Sending reply: success")
         result_server.send_json("success")
 except KeyboardInterrupt:
     logger.info("Keyboard interrupt. Shutting down.")
 finally:
-    conn.close() 
     result_server.unbind(result_address)
     context.term()
