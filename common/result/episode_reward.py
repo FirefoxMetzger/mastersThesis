a2d1185a36df87af61a2feaff6f40041b5a2b55e
Sebastian Wallkoetter <sebastian@wallkoetter.net>
2017-07-06 14:05:13 +0100

    started implementing result types

--- common/result/episode_reward.py
+++ common/result/episode_reward.py
@@ -3,6 +3,6 @@ from common.db_base import sql_base
 from common.task.experiment import Experiment
 
 class EpisodeReward(sql_base):
-    trial = peewee.ForeignKeyField(Experiment, related_name="Experiment")
+    trial = peewee.ForeignKeyField(Experiment)
     episode = peewee.BigIntegerField()
     reward = peewee.DoubleField()
---
+++ common/result/step_reward.py
@@ -0,0 +1,18 @@
+import os
+import peewee
+from playhouse.db_url import connect
+from common.db_base import sql_base
+from common.task.experiment import Experiment
+
+class StepReward(sql_base):
+    trial = peewee.ForeignKeyField(Experiment)
+    episode = peewee.BigIntegerField()
+    reset = peewee.BigIntegerField()
+    step = peewee.BigIntegerField()
+    reward = peewee.DoubleField()
+    
+    class Meta():
+        db = connect(os.environ["DB_ADDRESS"])
+        indexes = (
+            (('trial', 'episode','step'), True),
+        )
