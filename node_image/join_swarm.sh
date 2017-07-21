a66d04c7b6a033c35622d3ea244511abac2f55bf
Sebastian Wallkoetter <sebastian@wallkoetter.net>
2017-07-21 15:26:27 +0100

    updated broken file

--- node_image/join_swarm.sh
+++ node_image/join_swarm.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Getting current manager join token"
 TOKEN=$( docker swarm join-token -q manager )
 
+echo "Joining managers into the swarm"
 while read IP; do
     printf "Now processing %s \n" "$IP"
     ssh-keygen -r $IP:22
@@ -12,3 +13,17 @@ docker swarm leave --force
 docker swarm join --token $TOKEN 192.168.0.40
 EOF
 done <managers.list
+
+echo "Getting current join join token"
+TOKEN=$( docker swarm join-token -q worker )
+
+echo "Joining workers into the swarm"
+while read IP; do
+    printf "Now processing %s \n" "$IP"
+    ssh-keygen -r $IP:22
+    ssh-keyscan -H $IP >> ~/.ssh/known_hosts
+    ssh docker@$IP /bin/sh << EOF 
+docker swarm leave --force
+docker swarm join --token $TOKEN 192.168.0.40
+EOF
+done <workers.list
