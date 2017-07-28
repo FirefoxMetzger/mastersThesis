4910833c57b293a4e769addf353b80fc89701f8c
Sebastian Wallkoetter <sebastian@wallkoetter.net>
2017-07-28 18:37:14 +0100

    optimized reducer
    reducer now pools episodes and submits in batches of approximately 200
    episodes.
    
    reducer now breaks out of the current message if it notices that it has
    collected all aviable steps for that episode.

--- pipelines/episode_reward/reducer/reduce.py
+++ pipelines/episode_reward/reducer/reduce.py
@@ -10,7 +10,7 @@ formatter = logging.Formatter(
         '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.setLevel("DEBUG")
+logger.setLevel("INFO")
 logger.debug("Set logging level to DEBUG")
 
 def create_socket(type_, address):
@@ -31,12 +31,17 @@ steps_received = set()
 accumulated_reward = 0
 steps_in_ep = -1
 
+episode_buffer = list()
+store_messages = False
+storage_counter = 0
+storage_cap = 200
+
 while not zmq.Context.instance().closed:
-    aviable = dict(poller.poll(3000))
+    aviable = dict(poller.poll(100))
     
     if active_topic == "":
         logger.debug("requesting a new topic to work on")
-        scheduler.send("")
+        scheduler.send_multipart(("new topic",""))
         logger.debug("waiting for a reply from scheduler")
         topic = scheduler.recv()
         if topic == "no work":
@@ -45,6 +50,20 @@ while not zmq.Context.instance().closed:
             logger.info("Now collecting topic: " + topic)
             active_topic = topic
             mapper_rep.setsockopt(zmq.SUBSCRIBE, topic)
+            
+        # every so many episodes, store them in bulk
+        # "no work" is included in counting to store the last eps when no new
+        # work is submitted
+        storage_counter += 1
+        if storage_counter >= storage_cap:
+            if episode_buffer:
+                logger.info("Storing %d new accumulated rewards" % len(episode_buffer))
+                EpisodeReward.insert_many(episode_buffer).upsert().execute()
+            episode_buffer = list()
+            storage_counter = 0
+    else:
+        scheduler.send_multipart(("heartbeat",active_topic))
+        scheduler.recv()
             
     if mapper_rep in aviable:
         topic, msg_json = mapper_rep.recv_multipart()
@@ -58,30 +77,35 @@ while not zmq.Context.instance().closed:
             if step["step"] not in steps_received:
                 accumulated_reward += step["reward"]
                 steps_received.add(step["step"])
+                
+                if steps_in_ep == -1:
+                    logger.debug("Topic %s -- collected %d/X steps." % (topic, len(steps_received)))
+                else:
+                    logger.debug("Topic %s -- collected %d/%d steps." % (topic, len(steps_received), steps_in_ep))
             
             if step["done"]:
                 logger.debug(step)
                 steps_in_ep = step["step"] + 1 # 0 indexed
             
-            if steps_in_ep == -1:
-                logger.debug("Topic %s -- collected %d/X steps." % (topic, len(steps_received)))
-            else:
-                logger.debug("Topic %s -- collected %d/%d steps." % (topic, len(steps_received), steps_in_ep))
+            if len(steps_received) >= steps_in_ep:
+                break   # all other steps will provide redundant information 
+                        # from the same ep being rescheduled upstream
         
         if len(steps_received) >= steps_in_ep:
             trial, episode = active_topic.split(",")
-            EpisodeReward.get_or_create(
-                trial=int(trial),
-                episode=int(episode),
-                reward = accumulated_reward)
-            logger.info("Episode %s of Experiment %s complete: Reward is %d after %d steps." % (trial, episode, accumulated_reward, len(steps_received)))
+            episode_buffer.append({"trial":trial, "episode":episode, "reward":accumulated_reward})
+            logger.info("Episode %s, Experiment %s, Reward %d, Steps: %d  -- Buffered %d/200" % 
+                        (trial, episode, accumulated_reward, len(steps_received), len(episode_buffer)))
+                        
+            scheduler.send_multipart(("done", active_topic))
+            scheduler.recv()
             
             mapper_rep.setsockopt(zmq.UNSUBSCRIBE, topic)
             active_topic = ""
             steps_received = set()
             accumulated_reward = 0
             steps_in_ep = -1
-            
+              
     elif not active_topic == "":
         logger.debug("Requesting more packets for topic: " + active_topic)
         reduce_req.send_multipart((active_topic, active_topic))
