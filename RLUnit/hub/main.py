97e9a3f9fe9c56c6e538ebf13334be9a8ea92601
Sebastian Wallkoetter <sebastian@wallkoetter.net>
2017-07-31 16:08:21 +0100

    added monitor counting messages

--- RLUnit/hub/main.py
+++ RLUnit/hub/main.py
@@ -1,13 +1,20 @@
 import zmq
-from zmq.devices import Proxy
+from zmq.devices import ProcessProxy as Proxy
 import os
 import logging
 
-logger = logging.getLogger()
-
-hub = Proxy(zmq.PULL, zmq.PUB)
+hub = Proxy(zmq.PULL, zmq.PUB, zmq.PUB)
 hub.bind_in(os.environ["WORKER_EVENT_ADDRESS"])
-logger.info("Collecting step from: %s" % os.environ["WORKER_EVENT_ADDRESS"])
 hub.bind_out(os.environ["PIPELINE_PUBLISHER_ADDRESS"])
-logger.info("Pushing to: %s" % os.environ["PIPELINE_PUBLISHER_ADDRESS"])
+hub.bind_mon("tcp://127.0.0.1:4008")
 hub.start()
+
+ctx = zmq.Context.instance()
+sock = ctx.socket(zmq.SUB)
+sock.connect("tcp://127.0.0.1:4008")
+sock.setsockopt(zmq.SUBSCRIBE, "step")
+msg_counter = 0
+while True:
+    sock.recv()
+    msg_counter += 1
+    print "Received %d messages" % (msg_counter)
