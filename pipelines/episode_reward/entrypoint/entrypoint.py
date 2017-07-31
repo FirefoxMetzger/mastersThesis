3847346814ef3156cd7f3eac2bf3bf56829c3439
Sebastian Wallkoetter <sebastian@wallkoetter.net>
2017-07-31 16:06:18 +0100

    added bigger high water mark to SUB
    further did some refactoring: made proxy run in seperate process
    added monitor that counts messages

--- pipelines/episode_reward/entrypoint/entrypoint.py
+++ pipelines/episode_reward/entrypoint/entrypoint.py
@@ -1,15 +1,25 @@
 import zmq
-from zmq.devices import Proxy
+from zmq.devices import ProcessProxy as Proxy
 import os
 import logging
 
 # just forward to mappers
 
 logger = logging.getLogger()
-proxy = Proxy(zmq.SUB, zmq.PUSH)
+proxy = Proxy(zmq.SUB, zmq.PUSH, zmq.PUB)
+proxy.setsockopt_in(zmq.RCVHWM, int(1e6))
 proxy.connect_in(os.environ["EXPERIMENT_EVENTS"])
-logger.info("Collecting step from: %s" % os.environ["EXPERIMENT_EVENTS"])
 proxy.setsockopt_in(zmq.SUBSCRIBE, "step")
 proxy.bind_out(os.environ["ENTRYPOINT_ADDRESS"])
-logger.info("Pushing to: %s" % os.environ["ENTRYPOINT_ADDRESS"])
+proxy.bind_mon("tcp://127.0.0.1:4008")
 proxy.start()
+
+ctx = zmq.Context.instance()
+sock = ctx.socket(zmq.SUB)
+sock.connect("tcp://127.0.0.1:4008")
+sock.setsockopt(zmq.SUBSCRIBE, "")
+msg_counter = 0
+while True:
+    sock.recv()
+    msg_counter += 1
+    print "Received %d messages" % (msg_counter)
