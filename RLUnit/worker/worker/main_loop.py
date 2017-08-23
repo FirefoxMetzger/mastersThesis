5222cf2f17b47a00b50e9eb6a8b8fd60e8f44a84
Sebastian Wallkoetter <sebastian@wallkoetter.net>
2017-08-23 12:47:36 +0100

    improved durability
    worker now uses a new connection to the distributor whenever it is
    requesting a new experiment. This prevents freezing in some cases where
    the distributor gets rescheduled to a different node.

--- RLUnit/worker/worker/main_loop.py
+++ RLUnit/worker/worker/main_loop.py
@@ -9,25 +9,25 @@ from orm import Experiment
 class WorkerLoop(threading.Thread):
     def __init__(self, context, queue_address):
         super(WorkerLoop, self).__init__()
-        
+
         # setup logger
         self.logger = logging.getLogger()
-        
+
         # setup class
         self.context = context
         self.process_another_experiment = True
         self.work_request_send = False
-        
+
         #setup command communication
         self.rep_cmd = context.socket(zmq.REP)
         self.rep_cmd.connect("inproc://loop_control")
-        
+
         #setup worker communication
         self.req_work = self.context.socket(zmq.REQ)
         self.req_work.setsockopt(zmq.LINGER,1)
-        self.req_work.connect(queue_address)
-        self.logger.info("Connected to %s to get work." % queue_address)
-        
+        self.queue_address = queue_address
+        #self.req_work.connect(queue_address)
+
         # init poller
         self.poller = zmq.Poller()
         self.poller.register(self.rep_cmd, zmq.POLLIN)
@@ -36,15 +36,17 @@ class WorkerLoop(threading.Thread):
     def run(self):
         while self.process_another_experiment:
             if not self.work_request_send:
+                self.req_work.connect(self.queue_address)
                 self.req_work.send("more")
                 self.work_request_send = True
-                
+
             else:
                 sock = dict(self.poller.poll(100))
-                
+
                 # process another experiment
                 if self.req_work in sock:
                     experiment_id = self.req_work.recv_json()
+                    self.req_work.disconnect(self.queue_address)
                     self.work_request_send = False
                     if experiment_id == "no work":
                         self.logger.debug("There is no work at this time. Sleeping for a while...")
@@ -54,14 +56,14 @@ class WorkerLoop(threading.Thread):
 
                         exp = Experiment(experiment_id, self.context)
                         exp.run()
-                        
+
                         self.logger.debug("Finished Task %s" % experiment_id)
-                
+
                 # if command msg handle it
                 if self.rep_cmd in sock:
                     cmd, msg = self.rep_cmd.recv_multipart()
                     self.handle_command_message(cmd, msg)
-            
+
     def handle_command_message(self, cmd, msg):
         if cmd == "stop":
             self.process_another_experiment = False
